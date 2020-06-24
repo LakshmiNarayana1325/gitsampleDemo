0eb766f764e5555ec28a22382c3ebbdf7beca625
lakshminarayana1325 <lakshminarayana1325.gmail.com>
2020-06-23 23:05:23 -0700

    person2 changes

--- src/gitexample/sample.java
+++ src/gitexample/sample.java
@@ -5,6 +5,7 @@ public class sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sample git proj");
+		System.out.println("sample git proj1");
 
 	}
 
